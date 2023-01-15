19df40cc1ec6fc344c4a955d70cbe384a81275bb
aldrinburgos <burgos.aldrin@gmail.com>
2023-01-15 17:28:09 -0600

    create and test pizza api routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -27,6 +27,36 @@ const pizzaController = {
         res.status(400).json(err);
       });
   },
+  //create a pizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+  //update pizza by id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this ID!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  //delete a pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this ID!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require("./routes"));
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt-v2",
   {
-    useFindandModify: false,
+    /* useFindAndModify: false, */
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
